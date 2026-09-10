7d597e1951b1e082b98d7185aff8e79498e4d9d5
Lakshay Aggarwal <lakshayagarwal2005@gmail.com>
2026-09-10 14:27:02 +0530

    fetch-project

---
+++ javascript/promise.js
@@ -0,0 +1,21 @@
+function test1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("task1");
+            resolve();
+        }, 4000);
+    });
+}
+
+function test2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("task2");
+            resolve();
+        }, 2000);
+    });
+}
+test1().then(test2)
+.catch((error) => {
+    console.error("Error occurred:", error);
+});
